09672bb600c6a968b0a05dff64e0d112048a1455
Marcus Overwater <moverwater@ethz.ch>
2025-12-08 21:34:14 +0100

    Updated validation xml

--- calibratedcpp-beast/validation/run_mcmc_inference.sh
+++ calibratedcpp-beast/validation/run_mcmc_inference.sh
@@ -10,7 +10,7 @@
 i=$SLURM_ARRAY_TASK_ID
 
 java --module-path=/cluster/software/stacks/2024-06/spack/opt/spack/linux-ubuntu22.04-x86_64_v3/gcc-12.2.0/javafx-20.0.1-3yh2tfmjuuvehv2uhgfvswuwzcushdow/lib \
-     --add-modules javafx.controls,javafx.fxml -jar $HOME/CalibratedCoalescentPointProcess.jar -seed $i \
+     --add-modules javafx.controls,javafx.fxml -jar calibratedcpp-beast-0.0.1.jar -seed $i \
      -statefile mcmc_inference_$i.state \
      -D Index=$i \
      -overwrite mcmc_inference.xml
