fb4be0f2406521604fc81ee9ea4463a7c0d34706
Marcus Overwater <moverwater@ethz.ch>
2026-02-03 15:00:22 +1300

    Removed old well calibrated simulation study files

--- calibratedcpp-beast/validation/calibratedcpp/well_calibrated_sim_study/sim_bd_rates.R
+++
@@ -1,7 +0,0 @@
-birthRates = runif(200,0,10)
-deathRates = runif(200,0,10)
-
-birthAndDeathRates = data.frame(birthRates = birthRates,
-                                deathRates = deathRates)
-
-write.csv(birthAndDeathRates,"~/code/beast_and_friends/CalibratedCoalescentPointProcess/validation/bd_rates.csv")
